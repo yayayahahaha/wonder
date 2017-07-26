02a9a014e421cd470efe1b6a69b795e0321e5d37
flyc <flychung@gmail.com>
2017-07-26 14:36:58 +0800

    remove all wonder's baseform

--- wonder/svg_polygon/BaseFormat/js/main.js
+++
@@ -1,9 +0,0 @@
-window.onload=function (argument) {
-	try{
-		$();
-		console.log("The JQuery and javascript are running!");
-	}catch(str){
-		console.error("The JQuery is NOT running! but javascript is running!!");
-	}
-
-}
